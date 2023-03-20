238cbafb2ae417c061625bb7049c1cf9afeaee11
Pedro Ledesma <rgzledesma@gmail.com>
2023-03-20 14:55:53 +0000

    Week4: setup a query

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool
+from lib.db import pool, query_wrap_array
 
 import logging #Cloudwatch logs
 
@@ -12,23 +12,38 @@ class HomeActivities:
   def run(cognito_user_id): # arg Logger
     #Logger.info("HomeActivities") #Cloudwatch logs. Carefull cost money
     # OpenTelemetry tracer setup
-    with tracer.start_as_current_span("home-activities-mock-data"): # Span caller
-      span = trace.get_current_span() # Span attributes 
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
+    #with tracer.start_as_current_span("home-activities-mock-data"): # Span caller
+     # span = trace.get_current_span() # Span attributes 
+      #now = datetime.now(timezone.utc).astimezone()
+      #span.set_attribute("app.now", now.isoformat())
 
-      sql = """
-      SELECT * FROM activitties
-      """
+      sql = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
+
       with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
           json = cur.fetchone()
-      print("==1------")
-      print("==------")
-      print(json)
+          #json = cur.fetchall()
+      print("---------")
+      print(json[0])
+    
       return json[0]
       return results
 
