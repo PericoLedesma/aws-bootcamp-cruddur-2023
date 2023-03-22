d492e15056c5cbf56f976f5e607a7a2eb9f77f7b
Pedro Ledesma <rgzledesma@gmail.com>
2023-03-22 09:57:44 +0000

    Week4: lamdba config

--- aws/lamdbas/cruddur-post-confirmation.py
+++ aws/lamdbas/cruddur-post-confirmation.py
@@ -1,48 +1,35 @@
 import json
 import psycopg2
+import os
 
 def lambda_handler(event, context):
     print("Running lambda_handler")
     user = event['request']['userAttributes']
+    print('*userAttributes*')
+    print(user)
     
     user_display_name = user['name']
     user_email = user['email']
     user_handle = user['preferred_username']
     user_cognito_id = user['sub']
-    
-    try:
-        conn = psycopg2.connect(os.getenv('CONNECTION_URL')
-        cur = conn.cursor()
-                                
+
+    try:                                
         sql = f"""
-        "INSERT INTO users (
-        display_name, 
-        handle, 
-        cognito_user_id
-        ) 
-        VALUES(
-        {user_display_name},
-        {user_email},
-        {user_handle},
-        {user_cognito_id}
-        )"
-        """
-        '''                 
-        print('SQL Statement ----')
-        print(sql)
-        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-        cur = conn.cursor()
-        params = [
-          user_display_name,
-          user_email,
-          user_handle,
-          user_cognito_id
-        ]
-        cur.execute(sql,*params)
-        conn.commit() 
-        '''
-                                
-                                
+            INSERT INTO users(
+            display_name,
+            email, 
+            handle, 
+            cognito_user_id
+            ) 
+            VALUES(
+            {user_display_name},
+            {user_email},
+            {user_handle},
+            {user_cognito_id}
+            )
+        """ 
+        conn = psycopg2.connect(os.getenv('PROD_CONNECTION_URL'))
+        cur = conn.cursor()    
         cur.execute(sql)
         conn.commit() 
 
