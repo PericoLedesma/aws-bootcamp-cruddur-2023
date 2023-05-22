d6917cc456fc7ef92a99bd7e8f97629684db1c00
Pedro Ledesma <rgzledesma@gmail.com>
2023-05-22 10:41:08 +0000

    Week8 starting! SDK

---
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3'
+
+
+export class ThumbingServerlessCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+
+  }
+
+  createBucket(bucketName: string){
+    const  bucket = new s3.
+
+
+  }
+
+
+
+}
