6c0765e9148de8a20cb89300cef8d4204ec1e87f
Pedro Ledesma <rgzledesma@gmail.com>
2023-05-22 14:17:55 +0000

    Week8 cdk

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,6 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import { LambdaInvokeAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as dotenv from 'dotenv';
+
+
+dotenv.config();
+
 
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
@@ -9,18 +16,37 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const functionPath: string = process.env.THUMBING_BUCKET_PATH as string;
+    const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
+
     const bucket = this.createBucket(bucketName);
+    const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
 
   }
 
   createBucket(bucketName: string): s3.IBucket {
     const  bucket = new s3.Bucket(this,'ThumbingBucket',{
       bucketName: bucketName,
-      removalPolicy: cdk.removalPolicy.DESTROY
+      removalPolicy: cdk.RemovalPolicy.DESTROY
     });
     return bucket;
   }
 
+  createLambda(functionPath: string, folderInput: string, folderOutput: string): lambda.IFunction {
+    const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(functionPath),
+      enviroments: {
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderInput,
+        FOLDER_OUTPUT: folderOutput,
+        PROCESS_WIDTH: '512',
+        PROCESS_WEIGHT: '512'
+      }
+    });
+    return lambdaFunction;
+  }
 
 
 }
